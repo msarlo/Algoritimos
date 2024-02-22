1596cc61ba708f37b78cde328daea78d595081da
msarlo <mateussarlo1@outlook.com>
2024-02-22 01:02:08 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿ double baseret;
+ double alturaret;
+ double area ;
+
+Console.Write("Escreva a base do retângulo: ");
+baseret = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Escreva a altura do retângulo: ");
+alturaret = Convert.ToInt32(Console.ReadLine());
+
+area = baseret * alturaret;
+
+Console.Write("A área é " + area );
+
+
