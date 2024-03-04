76d3c26a0ad27b8fde46254dd855d394f2fe1f02
msarlo <mateussarlo1@outlook.com>
2024-03-03 23:22:20 -0300

    adcionando novo projeto

---
+++ Operadores relacionias/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nome;
+        int idade;
+        char sexo;
+        bool reservista;
+
+        Console.Write("qual seu nome?: ");
+        nome = Console.ReadLine();
+
+        Console.Write("qual a sua idade: ");
+        idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("qual a seu sexo: ");
+        sexo = Convert.ToChar(Console.ReadLine().ToUpper()); 
+
+        reservista = idade >= 18 && sexo == 'M';
+
+        if (reservista)
+        {
+            Console.WriteLine("Faça seu cadastro {0} ",  nome );
+        }
+        else
+        {
+            if(sexo == 'f')
+            { 
+            Console.Write("mulheres não precisam se cadastrar");
+            }
+            
+             else
+            {
+            Console.WriteLine("Aguarde completar 18 anos {0}",nome);
+            } 
+        }
+    }
+}
---
+++ Primeiro projeto/Program.cs
@@ -0,0 +1,29 @@
+﻿ double baseret;
+ double alturaret;
+ double area ;
+
+Console.Write("Escreva a base do retângulo: ");
+baseret = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Escreva a altura do retângulo: ");
+alturaret = Convert.ToInt32(Console.ReadLine());
+
+area = baseret * alturaret;
+
+Console.Write("A área é " + area );
+
+
+    /* double porcentagem;
+     double num;
+     double resultado; 
+
+    Console.Write("escolha a porcentagem desejada: ");
+    porcentagem = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("escolha um número real: ");
+    num = Convert.ToDouble(Console.ReadLine());
+
+    resultado = num/100 * porcentagem ;
+
+    Console.Write("o resultado é " + resultado);
+*/
